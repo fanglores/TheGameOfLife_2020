519d1c75da04f4c99fd30c59908c594389064752
Konstantin <72685417+fanglores@users.noreply.github.com>
2020-10-12 19:34:57 +0300

    Fix of updated GoL.cpp    (working)
    -Fixed vector subscript out of range in different places. 
    -Fixed bugs
    
    +Added infinity evolution
    +Added detection of stagnation(end of changing)
    +Added stable input of the command in menu()
    
    Everything works 
    
    To do: detector of cycles(stagnation in 2 states)

--- GoL.cpp
+++ GoL.cpp
@@ -5,166 +5,203 @@
 using namespace std;
 
 int N, G;
-vector<vector<bool>> cells(N, vector<bool>(N, false));
+bool stagnation = false;
+vector<vector<bool>> cells;
 
 
 bool get_next_state(int y, int x)
 {
-  bool up,down,left,right;
-  int neighbors = 0;
-  
-  if (x == 0) up = true;
-  else up = false;
-  if (x == N - 1) down = true;
-  else down = false;
-  if (y == 0) left = true;
-  else left = false;
-  if (y == N - 1) right = true;
-  else right = false;
+    bool up, down, left, right;
+    int neighbors = 0;
 
-  if(!up)
-  {
-    neighbors += int(cells[y - 1][x]);
-    if (!left) neighbors += int(cells[y - 1][x - 1]);
-    if (!right) neighbors += int(cells[y - 1][x + 1]);
-  }
-  if(!down)
-  {
-    neighbors += int(cells[y + 1][x]);
-    if (!left) neighbors += int(cells[y + 1][x - 1]);
-    if (!right) neighbors += int(cells[y + 1][x + 1]);
-  }
-  if (!right) neighbors += int(cells[y][x + 1]);
-  if (!left) neighbors += int(cells[y][x - 1]);
+    if (y == 0) up = true;
+    else up = false;
+    if (y == N - 1) down = true;
+    else down = false;
+    if (x == 0) left = true;
+    else left = false;
+    if (x == N - 1) right = true;
+    else right = false;
 
-  if(cells[y][x]) 
-  {
-    if(neighbors >= 2 && neighbors <= 3) return true;    //alive cell keeps living
-    else return false;                       //alive cell dies
-  }
-  else 
-  {
-    if(neighbors == 3) return true;                 //dead cell resurrected
-    else return false;                        //dead cell is dead
-  }
-  
+    if (!up)
+    {
+        neighbors += int(cells[y - 1][x]);
+        if (!left) neighbors += int(cells[y - 1][x - 1]);
+        if (!right) neighbors += int(cells[y - 1][x + 1]);
+    }
+    if (!down)
+    {
+        neighbors += int(cells[y + 1][x]);
+        if (!left) neighbors += int(cells[y + 1][x - 1]);
+        if (!right) neighbors += int(cells[y + 1][x + 1]);
+    }
+    if (!right) neighbors += int(cells[y][x + 1]);
+    if (!left) neighbors += int(cells[y][x - 1]);
+
+    if (cells[y][x])
+    {
+        if (neighbors >= 2 && neighbors <= 3) return true;    //alive cell keeps living
+        else return false;                       //alive cell dies
+    }
+    else
+    {
+        if (neighbors == 3) return true;                 //dead cell resurrected
+        else return false;                        //dead cell is dead
+    }
+
 }
 
 bool auto_init()
 {
-  int n, i, x, y;
-  system("cls");
-  srand(time(0));
-  cout << "********************AUTO_INIT********************\n\n";
-  
-  cout << "Enter number of cells to init (-1 for automatical generation):\n>>";
-  cin >> n;
-  
-  if (n == -1) n = N / 3 + N % 3;
-  cout << "\n$Initializing " << n << " cells...\n\n";
-  
-  while (i < n)
-  {
-    x = rand() % N;
-    y = rand() % N;
-    
-    if (cells[y][x]) continue;
-    else
+    int n, i = 0, x, y;
+    system("cls");
+    srand(time(0));
+    cout << "*************************AUTO_INIT*************************\n\n";
+
+    cout << "Enter number of cells to init (-1 for automatical generation):\n>>";
+    cin >> n;
+
+    if (n == -1) n = N * N / 3 + N % 3;
+    cout << "\n$Initializing " << n << " cells...\n\n";
+
+    while (i < n)
     {
-      cells[y][x] = true;
-      i++;
+        x = rand() % N;
+        y = rand() % N;
+
+        if (cells[y][x]) continue;
+        else
+        {
+            cells[y][x] = true;
+            i++;
+        }
     }
-  }
-  
-  cout << "Initialization successful!\n";
-  return true;
+
+    cout << "Initialization successful!\n";
+    return true;
 }
 
 bool manual_init()
 {
-  system("cls");
-  int x, y;
-  cout << "********************MANUAL_INIT********************\n\n";
-    
-  cout << "Enter line and column numbers (-1 -1 as end of input):\n>>";
-  cin >> y >> x;
-  
-  while(x != -1)
-  {
-    cells[y][x] = true;
-    cout << "\n>>";
+    system("cls");
+    int x, y;
+    cout << "*************************MANUAL_INIT*************************\n\n";
+
+    cout << "Enter line and column numbers (-1 -1 as end of input):\n>>";
     cin >> y >> x;
-  }
-  
-  cout << "\n\nInitialization successful!\n";
-  return true;
+
+    while (x != -1)
+    {
+        cells[y][x] = true;
+        cout << ">>";
+        cin >> y >> x;
+    }
+
+    cout << "\n\nInitialization successful!\n";
+    return true;
 }
 
 void evolve()
 {
-  vector<vector<bool>> sub(N, vector<bool>(N));
-  
-  for(int i = 0; i < N; i++)
-    for(int j = 0; j < N; j++)
-      sub[i][j] = get_next_state(i, j);
-  
-  cells = sub;
+    vector<vector<bool>> sub(N, vector<bool>(N));
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            sub[i][j] = get_next_state(i, j);
+ 
+    stagnation = (cells == sub);
+    cells = sub;
 }
 
 bool menu()
 {
-  system("cls");
-  string ans;
-  cout << "Enter size of the test field:\n>>";
-  cin >> N;
-  cout << "\nEnter number of the generations (-1 for infinity):\n>>";
-  cin >> G;
-  cout << "\nSelect type of initialization:\n\trand: automatic initialization\n\tman: manual initialization\n\n>>";
-  cin >> ans;
-  
-  if(ans == "rand") return auto_init();
-  else if(ans == "man") return manual_init();
-  else 
-  { 
-    cout << "Fatal error: Command was not recognized" << endl; 
-    return false;
-  }
+    system("cls");
+    string ans;
+    cout << "Enter size of the test field:\n>>";
+    cin >> N;
+
+    cells.resize(N, vector<bool>(N, false));
+
+    cout << "\nEnter number of the generations (-1 for infinity):\n>>";
+    cin >> G;
+
+    while (true)
+    {
+        cout << "\nSelect type of initialization:\n\trand: automatic initialization\n\tman: manual initialization\n\n>>";
+        cin >> ans;
+    
+        if (ans == "rand") return auto_init();
+        else if (ans == "man") return manual_init();
+        else
+        {
+            cout << "Fatal error: Command was not recognized" << endl;
+            system("pause");
+            system("cls");
+        }
+    }
 }
 
 void print()
 {
-  for (vector<bool> v : cells) 
+    for (int i = 0; i < N; i++) cout << " _";
+    cout << endl;
+    for (vector<bool> v : cells)
     {
-      for (bool a : v)
-        if(a) cout << "|*";
-        else cout << "| ";
-      cout << "|\n";
+        for (bool a : v)
+            if (a) cout << "|*";
+            else cout << "| ";
+        cout << "|\n";
     }
 }
 
 
 int main()
 {
-  
-  if(menu())          //init
-  {
-    system("pause");
-    system("cls");
-    
-    cout << "0 generation state:\n\n";
-    print();
-  
-    //make different way for N == -1
-    for(int i = 0; i < G; i++) evolve();
 
-    cout << "\n\n" << N << " generation state:\n\n";
-    print();
-    return 1;
-  }
-  else 
-  { 
-    system("pause"); 
-    return 0; 
-  }
-  
+    if (menu())          //init
+    {
+        system("pause");
+        system("cls");
+
+        cout << "0 generation state:\n";
+        print();
+        
+        int i = 0;
+        bool infinitive = (G == -1);
+
+        if (!infinitive)
+        {
+            while (i < G && !stagnation)
+            {
+                evolve();
+                i++;
+            }
+            cout << "\n\n" << N << "th generation state:\n";
+            print();
+        }
+        else 
+            while (!stagnation)
+            {
+                system("pause");
+                evolve();
+                i++;
+                cout << "\n" << i << " generation state:\n";
+                print();
+            }
+
+        if (stagnation)
+        {
+            cout << "\n$Found end of the world`s evolution!";
+            cout << "\n" << i << "th generation state:\n";
+            print();
+        }
+
+        return 1;
+    }
+    else
+    {
+        system("pause");
+        return 0;
+    }
+
 }
